272438cbdbf17314f9df2a10b59a2b4181126037
Sergio Patron <danilopatron1982@gmail.com>
2019-04-01 16:05:41 -0600

    Changing the flow order (finite state machine)

--- src/com/g2/vending_machine/MainApplication.java
+++ src/com/g2/vending_machine/MainApplication.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import com.g2.vending_machine.controllers.*;
 import com.g2.vending_machine.models.Product;
+import com.g2.vending_machine.utils.ScreenUtils;
 
 public class MainApplication {
 
@@ -14,42 +15,46 @@ public class MainApplication {
 		ProductController pc = null;
 		Map<String, Product> productList = null;
 		int option;
+		String message = "";
 		
 		do {
-			option = mainMenu();
+			ScreenUtils.clearScreen();
+			option = mainMenu(message);
+			message = "";
 			
 			switch(option) {
 				case 1:
+					ScreenUtils.clearScreen();
 					fc = FileController.getInstance(fileUploaded());
 					productList = fc.getProductList();
+					message = String.format("%d Product(s) successfully uploaded.", productList.size());
 					break;
 				case 2:
+					ScreenUtils.clearScreen();
 					pc = ProductController.getInstance(productList);
-					pc.showProductList();
-					pc.showProductOptions();
+					message = pc.handleProductTransaction();
 					break;
 				default:
-					System.out.println("It's not an option, please, try again.");
+					ScreenUtils.clearScreen();
+					message = String.format("It's not an option, please, try again.");
 					break;
 			}
 			
 		} while(option != 0);
 	}
 	
-	private static int mainMenu() {
-		
-		System.out.println("Vending Machine");
+	private static int mainMenu(String message) {
+		System.out.printf("%s\n\n", message);
 		System.out.println("0 Exit");
 		System.out.println("1 Upload product list");
 		System.out.println("2 Buy a product");
-
+		System.out.print("Option: ");
 		Scanner scan = new Scanner(System.in);
 		return scan.nextInt();
 	}
 
 	private static String fileUploaded() {
-		System.out.println("Insert the path + file name: ");
-		
+		System.out.print("Insert the path + file name: ");
 		Scanner scan = new Scanner(System.in);
 		return scan.nextLine();
 	}
